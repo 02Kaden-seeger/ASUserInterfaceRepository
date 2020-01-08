755f726410c5d13d07bab9827dcda5327436eb9f
Kaden <02kaden.seeger@gulllakecs.org>
2020-01-08 10:26:27 -0500

    made the button actually go up

--- app/src/main/java/com/example/asuserinterface/MainActivity.java
+++ app/src/main/java/com/example/asuserinterface/MainActivity.java
@@ -1,17 +1,18 @@
 package com.example.asuserinterface;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 /**
 
  This app displays an order form to order coffee
  */
 public class MainActivity extends AppCompatActivity {
-
+    int numberOfCoffees = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +24,9 @@ public class MainActivity extends AppCompatActivity {
      This method is called when the order is clicked.
      */
     public void submitOrder(View view) {
-        display(1);
+        numberOfCoffees++;
+        display(numberOfCoffees);
+        displayPrice(numberOfCoffees * 5);
     }
     /**
 
@@ -32,5 +35,9 @@ public class MainActivity extends AppCompatActivity {
     private void display(int number) {
         TextView quantityTextView = findViewById(R.id.zero);
         quantityTextView.setText("" + number);
+    }
+    private void  displayPrice(int number){
+        TextView PriceTextView=(TextView)findViewById(R.id.Price);
+        PriceTextView.setText(NumberFormat.getCurrencyInstance(new Locale("en", "US")).format(number));
     }
 }
